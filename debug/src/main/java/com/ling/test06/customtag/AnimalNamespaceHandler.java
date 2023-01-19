fb9e5815796e978439f65faa3216778ddda48967
zhangling <390597591@qq.com>
2023-01-19 21:30:23 +0800

    解析自定义标签

--- debug/src/main/java/com/ling/test06/customtag/AnimalNamespaceHandler.java
+++ debug/src/main/java/com/ling/test06/customtag/AnimalNamespaceHandler.java
@@ -3,13 +3,13 @@ package com.ling.test06.customtag;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
 /**
- * Animal 命名空间处理
+ * Animal 命名空间初始化
  */
 public class AnimalNamespaceHandler extends NamespaceHandlerSupport {
 
 	@Override
 	public void init() {
-		// 遇到自定义标签 <cat:aaa 这样以 cat 开头的元素，将该元素交给 CatBeanDefinitionParser 解析
+		// 解析 animal 命名空间下的 cat 标签 <animal:cat 标签
 		registerBeanDefinitionParser("cat", new CatBeanDefinitionParser());
 		registerBeanDefinitionParser("dog", new DogBeanDefinitionParser());
 	}
